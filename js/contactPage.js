4da8d50cb107f241899e2d65f40351ebafd857f6
akkermans99 <44165501+akkermans99@users.noreply.github.com>
2018-10-17 22:03:17 +0200

    Add files via upload

--- js/contactPage.js
+++ js/contactPage.js
@@ -15,18 +15,28 @@ $(document).ready(function () {
         $('#mySearch').autocomplete({
             source: id_list
         });
-               
+
     }
 });
-
-$("#btnSearch").click(function(e) {
-    e.preventDefault();
-    var searchValue = $("#mySearch").val();
-    if ( id_list.includes(searchValue) ) {
-        $('#' + searchValue)[0].scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    }
+$(function() {
+    $("#btnSearch").click(function (e) {
+        e.preventDefault();
+        var searchValue = $("#mySearch").val();
+        if (id_list.includes(searchValue)) {
+            $('#' + searchValue)[0].scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        }
+    });
+    
+    $('.card-contact').tooltip();
+    
+    $('.card-contact').click(function () {
+        console.log(this);
+        console.log($('#launch_' + this.dataset['modalid']));
+        var modalid = this.dataset['modalid'];
+        $('#' + modalid).modal();
+        $('#launch_' + this.dataset['modalid'])
+    });
 });
-
