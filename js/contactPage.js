4d6834debc8b80650cef659b2e4b7f6fc602f314
akkermans99 <44165501+akkermans99@users.noreply.github.com>
2018-10-17 16:05:17 +0200

    Add files via upload

--- js/contactPage.js
+++ js/contactPage.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
-    $('#navs').tabs();
+var id_list = [];
+
+$(document).ready(function () {
+    if ($('#mySearch')) {
+        $("h4").each(function () {
+            if (this.id !== "") {
+                id_list.push(this.id);
+            }
+        });
+        $("h5").each(function () {
+            if (this.id !== "") {
+                id_list.push(this.id);
+            }
+        });
+        $('#mySearch').autocomplete({
+            source: id_list
+        });
+               
+    }
 });
+
+$("#btnSearch").click(function(e) {
+    e.preventDefault();
+    var searchValue = $("#mySearch").val();
+    if ( id_list.includes(searchValue) ) {
+        $('#' + searchValue)[0].scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+});
+
--- js/eindOpdracht.js
+++ js/eindOpdracht.js
@@ -1,1 +1,18 @@
+$(document).ready(function () {
+    //if ($('#mySearch')) {
+        var id_list = $("h4").map(function () {
+                if (this.id !== "") {
+                    return this.id;
+                }
+            }).get();
+        
+        $("h5").each(function() {
+            if ( this.id !== "" ) {
+                id_list.push(this.id);
+            }
+        });
 
+        console.log(id_list);
+        
+   // }
+});
